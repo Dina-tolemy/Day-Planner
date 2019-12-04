802ac70c8b9a72ca48a690f80550fc334c91777e
Dinatolemy <dina.a.tolemy@gmail.com>
2019-12-04 16:53:19 +1100

    creating the html and js function for changing the input color as the hours change

---
+++ assets/script.js
@@ -0,0 +1,63 @@
+
+
+var currentTime=moment();
+chnagingInputColors();
+function chnagingInputColors(){
+if(currentTime.hour()==9)
+{
+
+    $('#userinput1').css('background-color', 'gray'); 
+}
+if(currentTime.hour()==10)
+{
+
+    $('#userinput1').css('background-color', 'gray'); 
+    $('#userinput2').css('background-color', 'yellow');
+}
+if(currentTime.hour()==11)
+{
+
+    $('#userinput1 ,#userinput2').css('background-color', 'gray'); 
+    $('#userinput3').css('background-color', 'yellow');
+}
+if(currentTime.hour()==12)
+{
+
+    $('#userinput1 ,#userinput2,#userinput3').css('background-color', 'gray'); 
+    $('#userinput4').css('background-color', 'yellow');
+}
+if(currentTime.hour()==13)
+{
+
+    $('#userinput1 ,#userinput2,#userinput3,#userinput4').css('background-color', 'gray'); 
+    $('#userinput5').css('background-color', 'yellow');
+}
+
+if(currentTime.hour()==14)
+{
+
+    $('#userinput1 ,#userinput2,#userinput3,#userinput4,#userinput5').css('background-color', 'gray'); 
+    $('#userinput6').css('background-color', 'yellow');
+}
+if(currentTime.hour()==15)
+{
+
+    $('#userinput1 ,#userinput2,#userinput3,#userinput4,#userinput5,#userinput6').css('background-color', 'gray'); 
+    $('#userinput7').css('background-color', 'yellow');
+}
+
+if(currentTime.hour()==16)
+{
+
+    $('#userinput1 ,#userinput2,#userinput3,#userinput4,#userinput5,#userinput6,#userinput7').css('background-color', 'gray'); 
+    $('#userinput8').css('background-color', 'yellow');
+}
+if(currentTime.hour()==17)
+{
+
+    $('#userinput1 ,#userinput2,#userinput3,#userinput4,#userinput5,#userinput6,#userinput7,#userinput8').css('background-color', 'gray'); 
+    $('#userinput9').css('background-color', 'yellow');
+}
+
+
+}
